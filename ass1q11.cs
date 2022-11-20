8b50009f60f0b5c162e9c54c6f7e38ca1ab03e9b
Deepika625 <118673564+Deepika625@users.noreply.github.com>
2022-11-20 14:24:45 +0000

    Assignment-1

---
+++ ass1q11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpassign
+{
+    internal class ass1q11
+    {
+        public static void Averagemarks()
+
+        {
+            Console.WriteLine("Average marks of 5 students:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+            int[] average = { a, b, c, d, e };
+            Console.WriteLine("maximum marks obtained are:" + average.Max());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ass1q2.cs
@@ -0,0 +1,51 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1
+    {
+        internal class Ass2
+        {
+            public static void main()
+            {
+                try
+                {
+                    int[] marks = new int[5];
+                    int sum = 0;
+                    float average;
+                    int highest = 0;
+                    Console.WriteLine("Enetr Marks:");
+                    for (int i = 0; i < marks.Length; i++)
+                    {
+                        marks[i] = int.Parse(Console.ReadLine());
+                    }
+                    for (int i = 0; i < marks.Length; i++)
+                    {
+                        sum += marks[i];
+                        if (marks[i] > highest)
+                        {
+                            highest = marks[i];
+                        }
+                    }
+                    average = sum / 5;
+                    Console.WriteLine("Average = (0) Highest(1)", average, highest);
+
+                }
+                catch (Exception Error)
+                {
+                    Console.WriteLine(Error.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("Final");
+                }
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+    
